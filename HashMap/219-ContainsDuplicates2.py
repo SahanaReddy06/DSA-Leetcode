a9e4b752b7858829bc61c5310e2cc4abdf21fee4
Sahana <reddysahana748@gmail.com>
2025-10-13 22:19:10 +0530

    Update 219-ContainsDuplicates2.py

--- HashMap/219-ContainsDuplicates2.py
+++ HashMap/219-ContainsDuplicates2.py
@@ -7,5 +7,15 @@ class Solution:
             index[num]=i
         return False
 
+
+#brute
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j] and abs(i - j) <= k:
+                    return True
+        return False
+
         
         
