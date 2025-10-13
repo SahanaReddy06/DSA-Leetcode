956cebaff0f1eddcd02ec68824e4e91537a28845
Sahana <reddysahana748@gmail.com>
2025-10-13 22:14:38 +0530

    Update 219-ContainsDuplicate2.py

--- SlidingWindow/219-ContainsDuplicate2.py
+++ SlidingWindow/219-ContainsDuplicate2.py
@@ -13,5 +13,16 @@ class Solution:
         return False
 
 
+#brute
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j] and abs(i - j) <= k:
+                    return True
+        return False
+
+
         
         
