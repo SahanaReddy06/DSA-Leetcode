5dfef7569b2953c75c90fc88f8372ed555a8d0ef
Sahana <reddysahana748@gmail.com>
2025-10-16 08:03:40 +0530

    Update 5-LongestPalindromeSubstring.py

--- 5-LongestPalindromeSubstring.py
+++ 5-LongestPalindromeSubstring.py
@@ -1,12 +1,12 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def expandAroundCenter(left:int, right:int)->str:
-            while left>=0 and right<len(s)-1 and s[left]==s[right]:
+            while left>=0 and right<len(s) and s[left]==s[right]:
                 left-=1
                 right+=1
             return s[left+1: right]
 
-        longest=" "
+        longest=""
         for i in range(len(s)):
             odd=expandAroundCenter(i, i)
             even=expandAroundCenter(i, i+1)
