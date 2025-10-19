cf6c10ff74038628c19183ce10b3cafb81dbdd38
Sahana <reddysahana748@gmail.com>
2025-10-19 22:34:32 +0530

    Update 21-Merge Two Sorted List.py

--- Linked List/21-Merge Two Sorted List.py
+++ Linked List/21-Merge Two Sorted List.py
@@ -6,21 +6,26 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy=ListNode(0)
-        head=dummy
+        tail=dummy
         while list1 and list2:
             if list1.val < list2.val:
-                head.next=list1       #if list1.val is greater it goes to list1
+                tail.next=list1       #if list1.val is greater it goes to list1
                 list1=list1.next      #list1 moves forward
             else:
-                head.next=list2
+                tail.next=list2
                 list2=list2.next
-            head=head.next
+            tail=tail.next
 
         if list1:
-            head.next=list1
+            tail.next=list1
         else:
-            head.next=list2
+            tail.next=list2
         return dummy.next
 
+#dummy → acts as a starting placeholder (so we don’t need to handle the first node separately).
+#tail → always points to the last node of the merged list.
+#while list1 and list2 → ensures both lists are processed until one becomes None.
+#tail.next = list1/list2 → connects the smaller node each time.
+#Finally, return dummy.next (actual merged head).
 
           
