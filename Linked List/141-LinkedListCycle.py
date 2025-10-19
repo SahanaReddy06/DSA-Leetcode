61c9bd1d19fe439d800af5b25af3cd1659881fab
Sahana <reddysahana748@gmail.com>
2025-10-19 21:16:27 +0530

    Update 141-LinkedListCycle.py

--- Linked List/141-LinkedListCycle.py
+++ Linked List/141-LinkedListCycle.py
@@ -15,3 +15,11 @@ class Solution:
                 return True
         return False
         
+
+#Initialize two pointers: slow and fast, both pointing to head.
+#Traverse the list while fast and fast.next are not None.
+#Move slow one step at a time (slow = slow.next).
+#Move fast two steps at a time (fast = fast.next.next).
+#Check if slow and fast meet:
+#If slow == fast, a cycle exists → return True.
+#If fast or fast.next becomes None, the list ends → return False.
