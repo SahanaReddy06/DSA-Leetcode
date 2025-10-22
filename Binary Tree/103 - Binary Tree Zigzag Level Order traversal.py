78cbb87d3f005191feecd564a1b23c72262308c3
Sahana <reddysahana748@gmail.com>
2025-10-22 19:01:42 +0530

    Create 103 - Binary Tree Zigzag Level Order traversal.py

---
+++ Binary Tree/103 - Binary Tree Zigzag Level Order traversal.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        res=[]
+        queue=deque([root])
+        while queue:
+            level_node=[]
+            for i in range(len(queue)):
+                node=queue.popleft()
+                level_node.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            level_node=list(reversed(level_node)) if len(res)%2 else level_node
+            res.append(level_node)
+        return res
+
+
+
+        
